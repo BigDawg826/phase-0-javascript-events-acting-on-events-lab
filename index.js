b4c3c25251bab951cda4c50b4e2666b10db1d954
Darian Fultz <dfultz826@yahoo.com>
2022-10-02 12:50:45 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ function moveDodgerLeft() {
     }
   }
 function moveDodgerRight() {
-    const rightNumbers = dodger.style.left.replace("px", "");
-    const right = parseInt(rightNumbers, 10);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
 
-    if (right < 360) {
-        dodger.style.left = `${right + 1}px`;
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
     }
   }
 document.addEventListener("keydown", function (e) {
     if (e.key === "ArrowLeft") 
       moveDodgerLeft();
     
-    else if (e.key === "ArrowRight") 
+    if (e.key === "ArrowRight") 
         moveDodgerRight();
     
   });
